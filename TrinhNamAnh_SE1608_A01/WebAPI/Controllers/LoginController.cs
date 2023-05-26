64e98e368caf738697ea6ee9ddd8462cc8f95193
quandoivietnam <trinhnamanh0909@gmail.com>
2023-05-26 08:05:08 +0700

    wtf is this bug

--- TrinhNamAnh_SE1608_A01/WebAPI/Controllers/LoginController.cs
+++ TrinhNamAnh_SE1608_A01/WebAPI/Controllers/LoginController.cs
@@ -17,10 +17,29 @@ namespace WebAPI.Controllers
         }
         // GET: api/<LoginController>
         [HttpGet]
-        public Customer Get()
+        public async Task<ActionResult<Category>> Login(string email, string password)
         {
+            if (string.IsNullOrEmpty(email))
+            {
+                return BadRequest("Email is null or empty");
+            }
+            Customer customer = null;
             var admin= Extension.Helper.ImportJson();
-            return admin;
+            if(admin.Email == email && admin.Password == password)
+            {
+                customer = admin;
+            }
+            else
+            {
+                var users = await _unitOfWork.CustomerService.Get();
+                var user = users.FirstOrDefault(x => x.Email == email && x.Password == password);
+                if(user != null)
+                {
+                    customer = user;
+                    return Ok(customer);
+                }
+            }
+            return BadRequest("Login failed please check email or password");
         }
     }
 }
